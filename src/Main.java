71556d2218d0395ebffb2affd328a50057b787f9
Peter Samadurau <spvrent@mail.ru>
2022-12-09 10:34:25 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
         System.out.println(formatter.format(new Date()));
-        LocalDate localDate = LocalDate.now();
+
         System.out.println("Hello world!");
 
     }
