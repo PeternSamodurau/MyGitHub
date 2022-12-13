ced2bce02f02975117dd67e5242210f45fa8c133
Peter Samadurau <spvrent@mail.ru>
2022-12-13 10:30:56 +0300

    scaner

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Scanner;
 import java.util.logging.SimpleFormatter;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
         System.out.println(formatter.format(new Date()));
 
         System.out.println("Hello world!");
-
+        String str = scanner.nextLine();
     }
 }
