3d9c7fa30bcadc4012f2fcacf3a9847da65340fd
Peter Samadurau <spvrent@mail.ru>
2022-12-21 11:36:35 +0300

    my-test-new branch2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        LocalDateTime localDateTime = LocalDateTime.of(1970,12,11,00,00,00);
 
     }
 }
