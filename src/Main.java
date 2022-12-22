ca47ba89ba8626c76487ebd7092e05cab6ea1175
Peter Samadurau <spvrent@mail.ru>
2022-12-22 10:32:37 +0300

    master22.12.22/1

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        LocalDateTime localDateTime = LocalDateTime.of(1970,12,11,00,00,00);
+        System.out.println("Hello world!!!!!!!!");
+        LocalDateTime localDateTime = LocalDateTime.of(2022,12,11,00,00,00);
 
         
 
