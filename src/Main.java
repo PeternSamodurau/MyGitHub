d53063967d612a04370119b8ca6b044330346cc3
Peter Samadurau <spvrent@mail.ru>
2022-12-22 10:47:35 +0300

    master22.12.22/2

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello world");
-        LocalDateTime localDateTime = LocalDateTime.of(2022,12,11,00,00,00);
-        System.out.println(localDateTime);
-        
 
-=======
         System.out.println("Hello world");
         LocalDateTime localDateTime = LocalDateTime.of(2022,12,11,00,00,00);
         System.out.println(localDateTime);
->>>>>>> new-contacts
+
+
 
     }
 }
